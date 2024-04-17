61f077f7456218f90d9b6cc918a6dee52b52e363
Roy <royzaye@gmail.com>
2024-04-18 00:50:08 +0930

    back up

---
+++ src/Routers.jsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import AllProductPage from "./components/AllProductPage";
+import Login from "./components/Auth/Login/index";
+import Profile from "./components/Auth/Profile";
+import Signup from "./components/Auth/Signup";
+import Blogs from "./components/Blogs";
+import Blog from "./components/Blogs/Blog.jsx";
+import CardPage from "./components/CartPage";
+import CheakoutPage from "./components/CheakoutPage";
+import Contact from "./components/Contact";
+import Faq from "./components/Faq";
+import FlashSale from "./components/FlashSale";
+import FourZeroFour from "./components/FourZeroFour";
+import Home from "./components/Home";
+import PrivacyPolicy from "./components/PrivacyPolicy";
+import ProductsCompaire from "./components/ProductsCompaire/index";
+import SallerPage from "./components/SallerPage";
+import Sallers from "./components/Sellers";
+import SingleProductPage from "./components/SingleProductPage";
+import TermsCondition from "./components/TermsCondition/index";
+import TrackingOrder from "./components/TrackingOrder";
+import Wishlist from "./components/Wishlist";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/all-products", element: <AllProductPage /> },
+  { path: "/single-product", element: <SingleProductPage /> },
+  { path: "/cart", element: <CardPage /> },
+  { path: "/checkout", element: <CheakoutPage /> },
+  { path: "/wishlist", element: <Wishlist /> },
+  { path: "/flash-sale", element: <FlashSale /> },
+  { path: "/saller-page", element: <SallerPage /> },
+  { path: "/products-compaire", element: <ProductsCompaire /> },
+  { path: "/sallers", element: <Sallers /> },
+  { path: "/about", element: <About /> },
+  { path: "/blogs", element: <Blogs /> },
+  { path: "/blogs/blog", element: <Blog /> },
+  { path: "/tracking-order", element: <TrackingOrder /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/faq", element: <Faq /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/profile", element: <Profile /> },
+  { path: "/privacy-policy", element: <PrivacyPolicy /> },
+  { path: "/terms-condition", element: <TermsCondition /> },
+  { path: "*", element: <FourZeroFour /> },
+]);
+
+function Routers() {
+  return <RouterProvider router={router} />;
+}
+
+export default Routers;
